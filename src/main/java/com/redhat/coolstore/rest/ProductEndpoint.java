65469cea24d201bb44a830d48b75b7f271fc86e9
hhpatel14 <hitpatel@redhat.com>
2025-04-15 16:25:44 -0400

    Fix by file with low effort

--- src/main/java/com/redhat/coolstore/rest/ProductEndpoint.java
+++ src/main/java/com/redhat/coolstore/rest/ProductEndpoint.java
@@ -3,10 +3,11 @@ package com.redhat.coolstore.rest;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
 import com.redhat.coolstore.model.Product;
 import com.redhat.coolstore.service.ProductService;
@@ -14,17 +15,17 @@ import com.redhat.coolstore.service.ProductService;
 @RequestScoped
 @Path("/products")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class ProductEndpoint implements Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -7227732980791688773L;
 
     @Inject
     private ProductService pm;
 
+    @Named
+    public ProductService getProductService() {
+        return pm;
+    }
 
     @GET
     @Path("/")
@@ -37,5 +38,4 @@ public class ProductEndpoint implements Serializable {
     public Product getProduct(@PathParam("itemId") String itemId) {
         return pm.getProductByItemId(itemId);
     }
-
 }
